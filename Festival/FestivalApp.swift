8eaeca4e3183d24bdd53df9a26dda50b083a84d4
Daniel Hallman <daniel.hallman@gmail.com>
2023-11-16 13:06:43 -0500

    Initial Commit

---
+++ Festival/FestivalApp.swift
@@ -0,0 +1,17 @@
+//
+//  FestivalApp.swift
+//  Festival
+//
+//  Created by Daniel Hallman on 11/16/23.
+//
+
+import SwiftUI
+
+@main
+struct FestivalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
